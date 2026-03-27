21d0963bcfbbbebf531e05fb77f08b2f2d52b4b1
Szogun538 <98102278+Szogun538@users.noreply.github.com>
2026-03-27 12:17:14 +0100

    Update install.sh
    Adding "wget" to dependecies

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 echo "Installing Neovim dependencies for Arch..."
 
 # The essentials for Telescope, Spectre, and Image Previews
-sudo pacman -S --needed ripgrep fd chafa imagemagick xclip ttf-jetbrains-mono-nerd
+sudo pacman -S --needed ripgrep fd chafa imagemagick xclip ttf-jetbrains-mono-nerd wget
 
 echo "Done! Now just open nvim and Lazy will do the rest."
